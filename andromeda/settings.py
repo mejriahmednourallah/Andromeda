1a10a697bc824e3bfe7fba8bfec13cfd55e43c7c
mejriahmednourallah <ahmednourallah.mejri@esprit.tn>
2025-10-23 18:20:54 +0100

    Change STATICFILES_STORAGE to CompressedStaticFilesStorage

--- andromeda/settings.py
+++ andromeda/settings.py
@@ -73,9 +73,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'core', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
